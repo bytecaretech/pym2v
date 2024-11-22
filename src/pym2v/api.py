64bf68d41f2e37902ae8a5a7ee9a9fd98ee060e0
morphZ <franz.kirchhoff@googlemail.com>
2024-11-22 14:38:37 +0100

    Enable auto token refreshing

--- src/pym2v/api.py
+++ src/pym2v/api.py
@@ -1,3 +1,5 @@
+from typing import Any
+
 from oauthlib.oauth2 import LegacyApplicationClient
 from requests_oauthlib import OAuth2Session
 
@@ -9,17 +11,28 @@ TOKEN_ROUTE = "/auth/realms/iiot-platform/protocol/openid-connect/token"
 class EurogardAPI:
     def __init__(self, settings: Settings = SETTINGS):
         self._settings = settings
+        self._token_url = settings.base_url + TOKEN_ROUTE
         self._session = self.create_session()
 
     def create_session(self) -> OAuth2Session:
+        extras: dict[str, Any] = {
+            "client_id": self._settings.client_id,
+            "client_secret": self._settings.client_secret,
+        }
+
         client = LegacyApplicationClient(client_id=self._settings.client_id)
-        oauth = OAuth2Session(client=client)
+        oauth = OAuth2Session(
+            client=client,
+            auto_refresh_url=self._token_url,
+            auto_refresh_kwargs=extras,
+            token_updater=lambda x: x,
+        )
+
         oauth.fetch_token(
-            token_url=self._settings.base_url + TOKEN_ROUTE,
+            token_url=self._token_url,
             username=self._settings.username,
             password=self._settings.password,
-            client_id=self._settings.client_id,
-            client_secret=self._settings.client_secret,
+            **extras,
         )
 
         return oauth
